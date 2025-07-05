74fdd8eac4a9737ad43460564ee2daad4b598818
lajusam <lajusammagar3@gmail.com>
2025-07-05 19:10:06 +0545

    assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './index.css'; 
 
-export const App = () => {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
-  
-      <h1>It's me Bibek </h1>
-  
-  )
-}
-export default App
+    <div className="user-container">
+      <h2>User List</h2>
+      <ul className="user-list">
+        {users.map((user) => (
+          <li key={user.id} className="user-card">
+            <h3>{user.name}</h3>
+            <p>{user.email}</p>
+            <p><em>{user.company.name}</em></p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
