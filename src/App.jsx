4dc38770096c84e32fa06f58059169f1a71e5d5a
lajusam <lajusammagar3@gmail.com>
2025-07-11 21:04:49 +0545

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import './index.css'; 
+// import React, { useState, useEffect } from 'react';
+// import './index.css'; 
+
+// const App = () => {
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((res) => res.json())
+//       .then((data) => setUsers(data));
+//   }, []);
+
+//   return (
+//     <div className="usercontainer">
+//       <h2>User List</h2>
+//       <ul className="userlist">
+//         {users.map((user) => (
+//      <li className="usercard">
+//             <h3>{user.name}</h3>
+        
+//          <p>{user.email}</p>
+//             <p><em>{user.company.name}</em></p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+import LoginPage from './pages/LoginPage'
+
 
 const App = () => {
-  const [users, setUsers] = useState([]);
+ 
+  return <LoginPage />
+}
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
-
-  return (
-    <div className="usercontainer">
-      <h2>User List</h2>
-      <ul className="userlist">
-        {users.map((user) => (
-     <li className="usercard">
-            <h3>{user.name}</h3>
-         <p>{user.email}</p>
-            <p><em>{user.company.name}</em></p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default App;
+export default App
