ed969f2155b3f8f4fd2c0f6d60a456682d2fd97a
sajinshr3stha <shresthasajin59@gmail.com>
2025-07-12 10:30:03 +0545

    complain form modal form

--- src/pages/ComplainFormModal.jsx
+++ src/pages/ComplainFormModal.jsx
@@ -1,11 +1,55 @@
 import { useState } from 'react'
-import { Button, Modal, Form, Input } from 'antd'
+import { Button, Modal, Form, Input, message, Upload } from 'antd'
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
 import axios from 'axios'
+
+const getBase64 = (img, callback) => {
+  const reader = new FileReader()
+  reader.addEventListener('load', () => callback(reader.result))
+  reader.readAsDataURL(img)
+}
+
+const beforeUpload = (file) => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG/PNG file!')
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!')
+  }
+  return isJpgOrPng && isLt2M
+}
 
 const ComplainFormModal = () => {
   const [loading, setLoading] = useState(false) // to track api calling process
   const [isModalOpen, setIsModalOpen] = useState(false)
   const token = localStorage.getItem('accessToken')
+
+  const [imageUrl, setImageUrl] = useState()
+
+  const handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true)
+      return
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (url) => {
+        setLoading(false)
+        setImageUrl(url)
+      })
+    }
+  }
+  const uploadButton = (
+    <button
+      style={{ border: 0, background: 'none' }}
+      type="button"
+    >
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  )
 
   const showModal = () => {
     setIsModalOpen(true)
@@ -73,6 +117,28 @@ const ComplainFormModal = () => {
             required
           >
             <Input />
+          </Form.Item>
+
+          <Form.Item>
+            <Upload
+              name="avatar"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList={false}
+              action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+              beforeUpload={beforeUpload}
+              onChange={handleChange}
+            >
+              {imageUrl ? (
+                <img
+                  src={imageUrl}
+                  alt="avatar"
+                  style={{ width: '100%' }}
+                />
+              ) : (
+                uploadButton
+              )}
+            </Upload>
           </Form.Item>
           {/* submit button  */}
           <Form.Item>
